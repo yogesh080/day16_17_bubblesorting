96c90ebaea7148ec1f394168fa7da2a6c894eda9
Yogesh <yogeshbietjhs@gmail.com>
2022-07-13 06:27:11 +0530

    inicail commit for bubblesorting

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿namespace BubbleSort_Day16_17
+{
+    class Program
+    {
+        public int[] numarr;
+        public Program()
+        {
+            numarr = new int[] { 87, 45, 7, 76, 46, 34, 85, 89, 32 };
+        }
+
+
+        public static void Main(string[] args)
+        {
+            Program program = new Program();
+            
+            program.BubbleSort(program.numarr);
+            Console.WriteLine("Array after Bubble Sort: ");
+            program.printArray();
+
+        }
+
+        public void BubbleSort(int[] arr)
+        {
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = 0; j < arr.Length - i - 1; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+            }
+
+        }
+
+        public void printArray()
+        {
+
+            foreach (int i in numarr)
+            {
+                Console.WriteLine(i);
+            }
+
+        }
+
+    }
+}
